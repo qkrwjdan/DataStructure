3253e08f39c3dbb9e28395696d312687fc051ada
qkrwjdan <madogisa12@naver.com>
2020-03-17 21:48:17 +0900

    Chapter 7 - (1),(2),(3)

---
+++ Chapter7/CircularQueueMain.c
@@ -0,0 +1,21 @@
+////
+//// Created by macbook air on 2020-03-17.
+////
+//
+//#include <stdio.h>
+//#include "CircularQueue.h"
+//
+//int main(void){
+//    Queue q;
+//    QueueInit(&q);
+//    int i;
+//
+//    for(i=0;i<98;i++){
+//        Enqueue(&q,i);
+//    }
+//    for(i=0;i<98;i++){
+//        printf("%d\n",Dequeue(&q));
+//    }
+//
+//    printf("%d\n",QPeek(&q));
+//}
