6ab9d4ada93956b8b0ece13f0029d5ded338e74c
qkrwjdan <madogisa12@naver.com>
2020-03-19 23:24:53 +0900

    Chapter 8 -(1),(2),(3)

---
+++ Chapter8/BinaryTree.h
@@ -0,0 +1,26 @@
+////
+//// Created by macbook air on 2020-03-18.
+////
+//
+//#ifndef CHAPTER8_BINARYTREE_H
+//#define CHAPTER8_BINARYTREE_H
+//
+//typedef int BTData;
+//
+//typedef struct _bTreeNode{
+//    BTData data;
+//    struct _bTreeNode * left;
+//    struct _bTreeNode * right;
+//}BTreeNode;
+//
+//BTreeNode * MakeBTreeNode(void);
+//BTData GetData(BTreeNode * bt);
+//void SetData(BTreeNode * bt, BTData data);
+//
+//BTreeNode * GetLeftSubTree(BTreeNode * bt);
+//BTreeNode * GetRightSubTree(BTreeNode * bt);
+//
+//void MakeLeftSubTree(BTreeNode * main,BTreeNode * sub);
+//void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+//
+//#endif //CHAPTER8_BINARYTREE_H
